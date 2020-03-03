09b37c0d4351cf5aa87147cab7d65a89e5b5ffd1
duarlgns1822@gmail.com <duarl@DESKTOP-RLI1BQT>
2020-03-03 22:51:52 +0900

    # 상수

---
+++ src/day_14/ArrayEx30.java
@@ -0,0 +1,20 @@
+/*
+ * # 상수
+ * 1. 변수 : 변할 수 있는 수
+ * 2. 상수
+ * 1) 변할 수 없는 수
+ * 2) 자료형 앞에 final을 붙인다.
+ * 3) 일반 변수와의 구분을 위해 변수명을 대문자로 작성한다.
+ */
+package day_14;
+
+public class ArrayEx30 {
+	public static void main(String[] args) {
+		int size = 9;
+		size = 1;
+		
+		final int MAX_SIZE = 9;
+		System.out.println(MAX_SIZE);
+		//final int MAX_SIZE = 1;			//에러 발생
+	}
+}
